dba3e4824f42db94f2a01a031bd308140627388b
laura <106522200+505laura@users.noreply.github.com>
2022-10-13 13:18:49 +0100

    Add a helper for basic questions

--- index.js
+++ index.js
@@ -12,42 +12,16 @@ const fs = require('fs');
 /** An array of possible licenses the user can choose from */
 const licenses = ['MIT', 'GPLv2', 'GPLv3', 'Apache', 'BSD', 'None'];
 
+const basicQuestion = (name, message) => ({type: 'input', name, message});
+
 /** Configuration for each question we want to ask the user */
-const titleQuestion = {
-    type: 'input',
-    name: 'title',
-    message: 'What is the title of your project?'
-};
-const descriptionQuestion = {
-    type: 'input',
-    name: 'description',
-    message: 'Please provide a description of your project.'
-};
-const installationQuestion = {
-    type: 'input',
-    name: 'installation',
-    message: 'Please provide installation instructions.'
-};
-const usageQuestion = {
-    type: 'input',
-    name: 'usage',
-    message: 'Please provide usage information.'
-};
-const contributionQuestion = {
-    type: 'input',
-    name: 'contribution',
-    message: 'Please provide contribution guidelines.'
-};
-const testQuestion = {
-    type: 'input',
-    name: 'test',
-    message: 'Please provide test instructions.'
-};
-const usernameQuestion = {
-    type: 'input',
-    name: 'username',
-    message: 'Please enter your GitHub username.'
-};
+const titleQuestion = basicQuestion('title', 'What is the title of your project?');
+const descriptionQuestion = basicQuestion('description', 'Please provide a description of your project.');
+const installationQuestion = basicQuestion('installation', 'Please provide installation instructions.');
+const usageQuestion = basicQuestion('usage', 'Please provide usage information.');
+const contributionQuestion = basicQuestion('contribution', 'Please provide contribution guidelines.');
+const testQuestion = basicQuestion('test', 'Please provide test instructions.');
+const usernameQuestion = basicQuestion('username', 'Please enter your GitHub username.');
 
 const emailQuestion = {
     type: 'input',
