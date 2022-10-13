74bd363d31bad790f237f2f610101d1902387c8c
laura <106522200+505laura@users.noreply.github.com>
2022-10-13 12:41:42 +0100

    Add more questions to prompt

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
 const inquirer = require('inquirer');
 
+const titleQuestion = {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?'
+};
+const descriptionQuestion = {
+    type: 'input',
+    name: 'description',
+    message: 'Please provide a description of your project.'
+};
+const installationQuestion = {
+    type: 'input',
+    name: 'installation',
+    message: 'Please provide installation instructions.'
+};
+const usageQuestion = {
+    type: 'input',
+    name: 'usage',
+    message: 'Please provide usage information.'
+};
+const contributionQuestion = {
+    type: 'input',
+    name: 'contribution',
+    message: 'Please provide contribution guidelines.'
+};
+const testQuestion = {
+    type: 'input',
+    name: 'test',
+    message: 'Please provide test instructions.'
+};
+const usernameQuestion = {
+    type: 'input',
+    name: 'username',
+    message: 'Please enter your GitHub username.'
+};
+
+const questions = [titleQuestion, descriptionQuestion, installationQuestion, usageQuestion, contributionQuestion, testQuestion, licenseQuestion, usernameQuestion, emailQuestion];
+
 inquirer
-    .prompt({
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of your project?'
-    })
+    .prompt(questions)
     .then((answers) => {
         console.log(answers)
     });
